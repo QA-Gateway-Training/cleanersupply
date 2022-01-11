641f204b307560acfb7e186e4576183e3b7e1bdd
waleedafifi90 <waleedafifi.blog@gmail.com>
2022-01-11 15:23:43 +0200

    header Search action without test

---
+++ Object Repository/Product details/btn_searchAction.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_searchAction</name>
+   <tag></tag>
+   <elementGuidId>8001fc5c-ebd7-4022-819c-dcfd3178f875</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div#header-search button.search-action</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Product details/input_searchField.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_searchField</name>
+   <tag></tag>
+   <elementGuidId>cb919904-d91f-48d5-a3a6-24044be0b30b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div#header-search input</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
