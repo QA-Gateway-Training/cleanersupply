b2e2f0cedc04b7e60df85868e93df578b9eabbee
RazanHammad97 <razanhammadq@gmail.com>
2022-01-14 15:30:27 +0300

    Add helper function to fill all checkout form page

---
+++ Object Repository/defaultFooter/div_topFooter.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_topFooter</name>
+   <tag></tag>
+   <elementGuidId>3e71f0f1-65cb-41ac-8af6-c63c322a20eb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#footer-container .footer-top</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
